17ad186bcd0d1e342c85f83a6637089a9a84aefb
unknown <sarah.lopriore@gmail.com>
2018-09-10 13:30:59 -0400

    create queryURL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
 
-    // create vraiables
-    var topics = ["avocado", "apple", "sweet potato", "bell pepper", "pineapple", "mango", "onion", "broccoli", "watermelon", "corn", "kiwi", "blueberry", "celery"]
+    // create topics variable
+    var topics = ["avocado", "apple", "sweet potato", "bell pepper", "pineapple", "mango", "onion", "broccoli", "watermelon", "corn", "kiwi", "blueberry", "celery"];
 
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=mMR97LCZTFVnxouA2wyRM7RKUTWNdMv7&limit=10&rating=g";
-
-
+ 
 
     // on page load create buttons for all items in the topics array
     // loop through the topics array to create each button
     for (i = 0; i < topics.length; i++) {
-        var $newButton = $("<button class='btn btn-success btn-space'></button>").append(topics[i]);
+        var $newButton = $("<button class='btn btn-success btn-space topics-btn'></button>").append(topics[i]);
+        $newButton.attr("id", topics[i]);
         $("#buttons").append($newButton);
-    }
+    };
 
     // for each button click, run an ajax request to the giphy api to get 10 search results for that topic
+    $(document).on("click", ".topics-btn", function() {
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).attr("id") + "&api_key=mMR97LCZTFVnxouA2wyRM7RKUTWNdMv7&limit=10&rating=g";
+        console.log(queryURL);
+    })
     // each gif should also display the rating underneath
     // results are displayed as static gifs
     // have the gif results overwrite the previous results
