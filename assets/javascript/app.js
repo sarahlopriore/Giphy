1096de5390e805573cb5c3a2129a0eaa5ef2693a
unknown <sarah.lopriore@gmail.com>
2018-09-10 12:55:13 -0400

    add initial javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// create vraiables
+var topics = ["avocado", "apple", "sweet potato", "bell pepper", "pineapple", "mango", "onion", "broccoli", "watermelon", "corn", "kiwi", "blueberry", "celery"]
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=mMR97LCZTFVnxouA2wyRM7RKUTWNdMv7&limit=10&rating=g";
+
+
+
+// on page load create buttons for all items in the topics array
+// loop through the topics array to create each button
+
+// for each button click, run an ajax request to the giphy api to get 10 search results for that topic
+// each gif should also display the rating underneath
+// results are displayed as static gifs
+// have the gif results overwrite the previous results
+
+// when a static gif is clicked, it moves
+// when moving gif is clicked, it stops moving
+
+
+
+// when user adds an topic through the newButtonInput, it is added to the topics array
+// this then creates a new button for that new topic
+// this new button will function the same as the others
+
+
+
